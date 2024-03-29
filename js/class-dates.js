9dd4395dc377ab24b6f1893be70ed655d663679b
dbailey323 <159084521+dbailey323@users.noreply.github.com>
2024-03-29 09:06:56 +0000

    Update class-dates.js

--- js/class-dates.js
+++ js/class-dates.js
@@ -19,13 +19,13 @@ function displayClassDates(className, startDate, numWeeks, timeSuffix, skippedWe
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    displayClassDates("sih1", "2024-05-01", 7, "183000");
+    displayClassDates("sih1", "2024-05-01", 9, "183000");
     displayClassDates("snco", "2024-02-28", 9, "183000", [6]);
-    displayClassDates("s5jy", "2024-05-01", 7, "100000");
+    displayClassDates("s5jy", "2024-05-01", 9, "100000");
     displayClassDates("se7u", "2024-02-28", 9, "100000", [6]);
-    displayClassDates("stzh", "2024-01-05", 7, "093000");
+    displayClassDates("stzh", "2024-01-05", 9, "093000");
     displayClassDates("soh9", "2024-03-01", 9, "093000", [5]);
     displayClassDates("sgon", "2024-03-22", 6, "101500", [2]);
-    displayClassDates("si9s", "2024-05-03", 7, "093000");
+    displayClassDates("si9s", "2024-05-03", 9, "093000");
     displayClassDates("sflf", "2024-05-03", 1, "100000");
 });
